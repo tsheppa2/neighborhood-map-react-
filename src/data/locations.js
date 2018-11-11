232ea6f31dc35fa929a30058803cccfe8f40a129
tsheppa2 <37315585+tsheppa2@users.noreply.github.com>
2018-11-10 20:42:39 -0500

    initial commit

---
+++ src/data/locations.js
@@ -0,0 +1,15 @@
+export const locations = [
+  "Naples Beach Pier",
+  "Wynwood Walls",
+  "Vizcaya Museum & Gardens",
+  "The Ringling in Sarasota",
+  "The Hemingway Home",
+  "Pastel Victorian Houses in Seaside",
+  "The Beakers in Palm Beach",
+  "Walt Disney World",
+  "Lightner Museum in St. Augustine",
+  "Everglades National Park",
+  "The Dali Museum",
+  "Surfers at Jacksonville Beach",
+  "Casa Faena in Miami Beach"
+];
